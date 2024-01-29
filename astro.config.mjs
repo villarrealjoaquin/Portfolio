8d9709b9250072f2b07ffa5d59c956e27f73a056
Joaquinmv1 <villarrealjoaquin09@gmail.com>
2024-01-28 21:54:16 -0300

    feat: add translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-import preact from '@astrojs/preact';
 import react from '@astrojs/react';
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), react()]
+  integrations: [tailwind(), react(), astroI18next()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,9 @@
 export default {
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		// 'node_modules/flowbite-react/lib/esm/**/*.js',
 	],
 	theme: {
 		extend: {},
 	},
-	plugins: [
-		// require('flowbite/plugin'),
-	],
+	plugins: [],
 }
